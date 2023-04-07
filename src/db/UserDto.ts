ffdd58bf249e44b4ae3bd3eabbd52f5e19c4f6b9
Liliana V <morningstar@procorp.cz>
2023-04-07 15:18:02 +0200

    fix: correct checking in user update

--- src/db/UserDto.ts
+++ src/db/UserDto.ts
@@ -1,13 +1,16 @@
-import { User as BaseUser } from "@prisma/client"
+import {User as BaseUser} from "@prisma/client"
 
 export class UserDto {
     constructor(
         public id: string,
         public username: string,
         public pluralKey: string | null,
-    ) {}
+        public overridePluralId: string | null,
+        public admin: boolean,
+    ) {
+    }
 
     public static from(user: BaseUser): UserDto {
-        return new this(user.id, user.username, user.pluralKey ?? null);
+        return new this(user.id, user.username, user.pluralKey ?? null, user.overridePluralId ?? null, user.admin);
     }
 }
