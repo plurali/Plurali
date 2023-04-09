4a9e74bef21006c1beff8501ab303eeaef899fff
Liliana V <morningstar@procorp.cz>
2023-04-09 02:50:07 +0200

    feat: clear user cache

---
+++ packages/backend/src/services/redis/user.ts
@@ -0,0 +1,15 @@
+import { User } from "@prisma/client"
+import { $redis } from "."
+
+export const clearUserCache = async (user: string | User) => {
+    const id = typeof user === "string" ? user : user.id;
+    const keys = await $redis.keys(`plurali|@*|::${id}::|*`);
+
+    const transaction = $redis.multi();
+
+    for (const key of keys) {
+        transaction.del(key)
+    }
+
+    await transaction.exec();
+}
