5b28ffc22ca290524b2789c6158e12cf357ba3cf
Mia Vališová <lilianalillyy@gmail.com>
2023-04-23 04:07:41 +0200

    wip

--- packages/frontend/vite.config.ts
+++ packages/frontend/vite.config.ts
@@ -1,53 +1,27 @@
-import { Plugin, defineConfig } from 'vite';
+import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
 
-import crypto from 'crypto';
-import cheerio from 'cheerio';
-import fetch from 'node-fetch';
+// import crypto from 'crypto';
+// import cheerio from 'cheerio';
+// import fetch from 'node-fetch';
 
-function csp(): Plugin {
-  const HEAD = [
-    `{{$nonce := randAlphaNum 32}}`,
-    `{{.RespHeader.Set "Content-Security-Policy" (printf "default-src 'self'; script-src 'report-sample' 'self' https://cdn.tiny.cloud 'nonce-%[1]v'; style-src 'report-sample' 'nonce-%[1]v' 'self' https://cdn.tiny.cloud; base-uri 'self'; connect-src 'self'; font-src 'self'; frame-src 'self'; img-src 'self' blob: data: https://cdn.plurali.icu https://sp.tinymce.com https://cdn.tiny.cloud 'nonce-%[1]v'; manifest-src 'self'; media-src 'self';" $nonce)}}`,
-    `{{.RespHeader.Set "X-Nonce" $nonce}}`,
-  ].join("\n");
+// function csp(): Plugin {
+//   const HEAD = [
+//   ].join("\n");
 
-  return {
-    name: '@plurali/frontend-csp',
-    enforce: 'post',
-    apply: 'build',
-    transformIndexHtml: async (html, context) => {
-      const $ = cheerio.load(html);
-
-      const transform = async (element: cheerio.TagElement) => {
-        let attr = element.type === 'script' ? 'src' : 'href';
-        const src = element.attribs[attr];
-        let data = src.startsWith('http') ? await (await fetch(src)).text() : null;
-
-        if (!data) {
-          const item = context.bundle[element.attribs[attr].slice(1)] as any;
-          data = item?.code || item.source;
-        }
-
-        const hash = crypto.createHash('sha384').update(data).digest('base64');
-
-        element.attribs.nonce = '{{$nonce}}';
-        element.attribs.integrity = `sha384-${hash}`;
-      };
-
-      for (const script of $('script[src]').toArray() as cheerio.TagElement[]) {
-        await transform(script);
-      }
-
-      for (const stylesheet of $('link[rel=stylesheet][href]').toArray() as cheerio.TagElement[]) {
-        await transform(stylesheet);
-      }
-
-      return `${HEAD}${$.html()}`;
-    },
-  };
-}
+//   return {
+//     name: '@plurali/frontend-csp',
+//     enforce: 'post',
+//     apply: 'build',
+//     transformIndexHtml: async (html, context) => {
+//       return `${HEAD}${$.html()}`;
+//     },
+//   };
+// }
 
 export default defineConfig({
-  plugins: [vue(), csp()],
+  plugins: [vue()],
+  build: {
+    noncePlaceholder: '{{$nonce}}'
+  }
 });
