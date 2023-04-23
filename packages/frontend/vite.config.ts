dd95ee899377818bf7e300763b660816ff5dfcb8
Mia Vališová <lilianalillyy@gmail.com>
2023-04-23 04:16:51 +0200

    not today

--- packages/frontend/vite.config.ts
+++ packages/frontend/vite.config.ts
@@ -1,27 +1,6 @@
 import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
 
-// import crypto from 'crypto';
-// import cheerio from 'cheerio';
-// import fetch from 'node-fetch';
-
-// function csp(): Plugin {
-//   const HEAD = [
-//   ].join("\n");
-
-//   return {
-//     name: '@plurali/frontend-csp',
-//     enforce: 'post',
-//     apply: 'build',
-//     transformIndexHtml: async (html, context) => {
-//       return `${HEAD}${$.html()}`;
-//     },
-//   };
-// }
-
 export default defineConfig({
   plugins: [vue()],
-  build: {
-    noncePlaceholder: '{{$nonce}}'
-  }
 });
