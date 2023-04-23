c1079dc0eb189eb95f7e9f31c7f38edc702cee1a
Mia Vališová <lilianalillyy@gmail.com>
2023-04-23 05:08:50 +0200

    chore: remove unused classes

--- packages/frontend/src/components/global/visibility/VisibilityTag.vue
+++ packages/frontend/src/components/global/visibility/VisibilityTag.vue
@@ -1,6 +1,6 @@
 <template>
   <button
-    class="text-sm text-gray-700 hover:opacity-75 uppercase p-1 px-4 text-white font-semibold rounded-2xl font-normal cursor-pointer transition"
+    class="text-sm hover:opacity-75 uppercase p-1 px-4 text-white font-semibold rounded-2xl cursor-pointer transition"
     :class="[
       visible ? 'bg-green-500' : 'bg-red-500',
       $attrs.disabled && 'cursor-disabled opacity-25 hover:opacity-25 bg-gray-500',
