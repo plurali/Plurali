0e6ee363797142d5b90f170c085b38ed862adc43
Mia Vališová <valisova@lilianaa.dev>
2023-11-18 01:12:59 +0100

    hotfix(backend): sort members alphabetically
    fixes #157

--- src/application/v1/controller/public/PublicSystemMemberController.ts
+++ src/application/v1/controller/public/PublicSystemMemberController.ts
@@ -66,7 +66,15 @@ export class PublicSystemMemberController {
       dtoMembers.push(dto);
     }
 
-    return dtoMembers;
+    return dtoMembers.sort(function (a, b) {
+      if (a.name < b.name) {
+        return -1;
+      }
+      if (a.name > b.name) {
+        return 1;
+      }
+      return 0;
+    });
   }
 
   @Get('/:memberId')
--- src/application/v1/controller/system/SystemMemberController.ts
+++ src/application/v1/controller/system/SystemMemberController.ts
@@ -91,7 +91,15 @@ export class SystemMemberController {
       dtoMembers.push(dto);
     }
 
-    return dtoMembers;
+    return dtoMembers.sort(function (a, b) {
+      if (a.name < b.name) {
+        return -1;
+      }
+      if (a.name > b.name) {
+        return 1;
+      }
+      return 0;
+    });
   }
 
   @UseGuards(SystemGuard)
