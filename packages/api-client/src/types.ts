327ebe233711536a379041dd636303123f766735
Mia Vališová <valisova@lilianaa.dev>
2023-12-25 15:44:16 +0100

    fix(api-client): replace * with named exports for types

--- packages/api-client/src/types.ts
+++ packages/api-client/src/types.ts
@@ -1,36 +1,48 @@
- export type * from "@plurali/pluraliapp/src/application/v2/dto/auth/AuthDtoInterface";
- export type * from "@plurali/pluraliapp/src/application/v2/dto/auth/request/AuthRequestInterface";
- export type * from "@plurali/pluraliapp/src/application/v2/dto/auth/request/RegisterRequestInterface";
- export type * from "@plurali/pluraliapp/src/application/v2/dto/auth/request/ResetPasswordRequestInterface";
+export type { AuthDtoInterface } from "@plurali/pluraliapp/src/application/v2/dto/auth/AuthDtoInterface";
+export type { AuthRequestInterface } from "@plurali/pluraliapp/src/application/v2/dto/auth/request/AuthRequestInterface";
+export type { RegisterRequestInterface } from "@plurali/pluraliapp/src/application/v2/dto/auth/request/RegisterRequestInterface";
+export type { ResetPasswordRequestInterface } from "@plurali/pluraliapp/src/application/v2/dto/auth/request/ResetPasswordRequestInterface";
 
- export type * from "@plurali/pluraliapp/src/application/v2/dto/field/FieldDataDtoInterface";
- export type * from "@plurali/pluraliapp/src/application/v2/dto/field/FieldDtoInterface";
- export type * from "@plurali/pluraliapp/src/application/v2/dto/field/ValueFieldDtoInterface";
- export type * from "@plurali/pluraliapp/src/application/v2/dto/field/request/UpdateFieldRequestInterface";
+export type { FieldDataDtoInterface } from "@plurali/pluraliapp/src/application/v2/dto/field/FieldDataDtoInterface";
+export type { FieldDtoInterface } from "@plurali/pluraliapp/src/application/v2/dto/field/FieldDtoInterface";
+export type { ValueFieldDtoInterface } from "@plurali/pluraliapp/src/application/v2/dto/field/ValueFieldDtoInterface";
+export type { UpdateFieldRequestInterface } from "@plurali/pluraliapp/src/application/v2/dto/field/request/UpdateFieldRequestInterface";
 
- export type * from "@plurali/pluraliapp/src/application/v2/dto/member/MemberDataDtoInterface";
- export type * from "@plurali/pluraliapp/src/application/v2/dto/member/MemberDtoInterface";
- export type * from "@plurali/pluraliapp/src/application/v2/dto/member/request/UpdateMemberRequestInterface";
+export type { MemberDataDtoInterface } from "@plurali/pluraliapp/src/application/v2/dto/member/MemberDataDtoInterface";
+export type { MemberDtoInterface } from "@plurali/pluraliapp/src/application/v2/dto/member/MemberDtoInterface";
+export type { UpdateMemberRequestInterface } from "@plurali/pluraliapp/src/application/v2/dto/member/request/UpdateMemberRequestInterface";
 
- export type * from "@plurali/pluraliapp/src/application/v2/dto/notification/NotificationDtoInterface";
+export type { NotificationDtoInterface } from "@plurali/pluraliapp/src/application/v2/dto/notification/NotificationDtoInterface";
 
- export type * from "@plurali/pluraliapp/src/application/v2/dto/page/PageDtoInterface";
- export type * from "@plurali/pluraliapp/src/application/v2/dto/page/request/CreatePageRequestInterface";
- export type * from "@plurali/pluraliapp/src/application/v2/dto/page/request/UpdatePageRequestInterface";
+export type { PageDtoInterface } from "@plurali/pluraliapp/src/application/v2/dto/page/PageDtoInterface";
+export type { CreatePageRequestInterface } from "@plurali/pluraliapp/src/application/v2/dto/page/request/CreatePageRequestInterface";
+export type { UpdatePageRequestInterface } from "@plurali/pluraliapp/src/application/v2/dto/page/request/UpdatePageRequestInterface";
 
- export type * from "@plurali/pluraliapp/src/application/v2/dto/partials/BackgroundDataInterface";
+export type { BackgroundDataInterface } from "@plurali/pluraliapp/src/application/v2/dto/partials/BackgroundDataInterface";
 
- export type * from "@plurali/pluraliapp/src/application/v2/dto/response/OkInterface";
- export * from "@plurali/pluraliapp/src/application/v2/dto/response/errors";
- export * from "@plurali/pluraliapp/src/application/v2/dto/response/warning";
+export type { OkInterface } from "@plurali/pluraliapp/src/application/v2/dto/response/OkInterface";
+export { ApiError, ApiErrorMessage } from "@plurali/pluraliapp/src/application/v2/dto/response/errors";
+export { ApiWarning, ApiWarningMessage } from "@plurali/pluraliapp/src/application/v2/dto/response/warning";
 
- export type * from "@plurali/pluraliapp/src/application/v2/dto/system/SystemDataDtoInterface";
- export type * from "@plurali/pluraliapp/src/application/v2/dto/system/SystemDtoInterface";
- export type * from "@plurali/pluraliapp/src/application/v2/dto/system/request/UpdateSystemRequestInterface";
+export type { SystemDataDtoInterface } from "@plurali/pluraliapp/src/application/v2/dto/system/SystemDataDtoInterface";
+export type { SystemDtoInterface } from "@plurali/pluraliapp/src/application/v2/dto/system/SystemDtoInterface";
+export type { UpdateSystemRequestInterface } from "@plurali/pluraliapp/src/application/v2/dto/system/request/UpdateSystemRequestInterface";
 
- export type * from "@plurali/pluraliapp/src/application/v2/dto/user/UserDtoInterface";
- export type * from "@plurali/pluraliapp/src/application/v2/dto/user/request/UpdateUserRequestInterface";
- export type * from "@plurali/pluraliapp/src/application/v2/dto/user/request/VerifyUserEmailRequestInterface";
+export type { UserDtoInterface } from "@plurali/pluraliapp/src/application/v2/dto/user/UserDtoInterface";
+export type { UpdateUserRequestInterface } from "@plurali/pluraliapp/src/application/v2/dto/user/request/UpdateUserRequestInterface";
+export type { VerifyUserEmailRequestInterface } from "@plurali/pluraliapp/src/application/v2/dto/user/request/VerifyUserEmailRequestInterface";
 
- export type * from "@plurali/pluraliapp/src/application/v2/types/request";
- export type * from "@plurali/pluraliapp/src/application/v2/types/response";
+export type { PaginationRequestQuery } from "@plurali/pluraliapp/src/application/v2/types/request";
+export type {
+  EntityDtoInterface,
+  IdentifiableEntityDtoInterface,
+  ApiResponseMeta,
+  ApiResponsePagination,
+  ApiResponseBase,
+  ApiResponseError,
+  ApiDataResponse,
+  ApiPaginatedDataResponse,
+  ApiErrorResponse,
+  ApiResponse,
+  ApiPaginatedResponse,
+} from "@plurali/pluraliapp/src/application/v2/types/response";
